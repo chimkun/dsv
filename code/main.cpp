6c2f5bc8af36dc2ec43c9d27ddae460af8a0cde5
chimkun <stevenhoangama@gmail.com>
2023-04-18 21:07:02 +0700

    update UI, fixing create function

--- code/main.cpp
+++ code/main.cpp
@@ -164,6 +164,7 @@ int main()
             }
         }
         window.clear();
+        mySLLObject.drawBackground(window);
         mySLLObject.processType(window);
         window.display();
     }
